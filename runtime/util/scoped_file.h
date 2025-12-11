d9b1e1e437690aeba774754dec375e4287e3638b
Google AI Edge <ai-edge-bot@google.com>
2025-12-11 14:38:48 -0800

    Have LiteRT LM depend on LiteRT's ScopedFile
    LiteRT-LM-PiperOrigin-RevId: 843379666

--- runtime/util/scoped_file.h
+++ runtime/util/scoped_file.h
@@ -15,97 +15,11 @@
 #ifndef THIRD_PARTY_ODML_LITERT_LM_RUNTIME_UTIL_SCOPED_FILE_H_
 #define THIRD_PARTY_ODML_LITERT_LM_RUNTIME_UTIL_SCOPED_FILE_H_
 
-#if defined(_WIN32)
-#include <Windows.h>
-#endif
-
-#include <cstddef>
-
-#include "absl/status/statusor.h"  // from @com_google_absl
-#include "absl/strings/string_view.h"  // from @com_google_absl
+#include "litert/cc/internal/scoped_file.h"  // from @litert
 
 namespace litert::lm {
 
-// A file wrapper that will automatically close on deletion.
-class ScopedFile {
- public:
-#if defined(_WIN32)
-  using PlatformFile = HANDLE;
-  static const PlatformFile kInvalidPlatformFile;
-#else
-  using PlatformFile = int;
-  static constexpr PlatformFile kInvalidPlatformFile = -1;
-#endif
-
-  static absl::StatusOr<ScopedFile> Open(absl::string_view path);
-  static absl::StatusOr<ScopedFile> OpenWritable(absl::string_view path);
-
-  ScopedFile() : file_(kInvalidPlatformFile) {}
-  explicit ScopedFile(PlatformFile file) : file_(file) {}
-  ~ScopedFile() {
-    if (IsValid()) {
-      CloseFile(file_);
-    }
-  }
-
-  ScopedFile(ScopedFile&& other) : file_(other.ReleasePlatformFile()) {}
-  ScopedFile& operator=(ScopedFile&& other) {
-    if (IsValid()) {
-      CloseFile(file_);
-    }
-    file_ = other.ReleasePlatformFile();
-    return *this;
-  }
-
-  ScopedFile(const ScopedFile&) = delete;
-  ScopedFile& operator=(const ScopedFile&) = delete;
-
-  PlatformFile file() const { return file_; }
-  bool IsValid() const { return file_ != kInvalidPlatformFile; }
-
-  // Returns the number of bytes of the file.
-  static absl::StatusOr<size_t> GetSize(PlatformFile file);
-  absl::StatusOr<size_t> GetSize() const { return GetSize(file_); }
-
-  // Returns a ScopedFile pointing to the same underlying file as `this`.
-  absl::StatusOr<ScopedFile> Duplicate();
-
-  // Releases ownership of the current file as a C file descriptor.
-  //
-  // Windows notes:
-  // Releases ownership of the operating system file HANDLE and returns the
-  // corresponding C file descriptor.
-  //
-  // Note: Currently, this function only works if the file can be re-opened in
-  // read/write mode.
-  //
-  // Warning: Files opened in asynchronous mode (`FILE_FLAG_OVERLAPPED`) are not
-  // supported. Windows' POSIX C implementation does not support such I/O
-  // operations. This function tries to detect such invalid use case and return
-  // an error but doesn't guarantee it.
-  //
-  // Warning: If successful, the returned file descriptor owns the file which
-  // means it will need to be closed using `_close`.
-  //
-  // Warning: While it is possible to get a HANDLE back from the file
-  // descriptor, **ownership will stay with the file descriptor**.
-  absl::StatusOr<int> Release();
-
- private:
-  PlatformFile ReleasePlatformFile() {
-    PlatformFile temp = file_;
-    file_ = kInvalidPlatformFile;
-    return temp;
-  }
-
-  // Platform-specific file operations requiring platform-specific
-  // implementations. It may be assumed by the implementation that the passed
-  // `PlatformFile` is valid. This must be ensured by the caller.
-  static void CloseFile(PlatformFile file);
-  static absl::StatusOr<size_t> GetSizeImpl(PlatformFile file);
-
-  PlatformFile file_;
-};
+using ScopedFile = ::litert::ScopedFile;
 
 }  // namespace litert::lm
 
